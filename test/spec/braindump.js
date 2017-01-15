cab587888b531497b6d94d4c45f80ba1c6761c9a
ahmad fahmy <ahmad fahmy>
2017-01-15 12:13:08 +0000

    added Jasmine and wired it all up.

---
+++ test/spec/braindump.js
@@ -0,0 +1,30 @@
+
+describe("Welcome Message(s)", function(){
+  it("Should give you a custom welcome message depending on time of day", function(){
+
+  });
+});
+
+describe("User Instructions", function(){
+  
+});
+
+describe("Unclassified Turds", function(){
+  
+});
+
+describe("Turdo's", function(){
+  
+});
+
+describe("Feelings", function(){
+  
+});
+
+describe("Custom Commands", function(){
+  
+});
+
+describe("User Experience", function(){
+  
+});
